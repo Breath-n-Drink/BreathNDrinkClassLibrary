0097c00cd24f0d26bc323a0c0c97d9203dc292ca
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2021-11-22 10:47:57 +0100

    Classes added

---
+++ AlcoholMeasurement.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BreathNDrinkClassLibrary
+{
+    public class AlcoholMeasurement
+    {
+        private double _measurement;
+        private string _userId;
+        private DateTime _timeStamp;
+
+        public double Measurement
+        {
+            get
+            {
+                return _measurement;
+            }
+            set
+            {
+                _measurement = value;
+            }
+        }
+
+        public string UserId
+        {
+            get
+            {
+                return _userId;
+            }
+            set
+            {
+                _userId = value;
+            }
+        }
+
+        public DateTime TimeStamp
+        {
+            get
+            {
+                return _timeStamp;
+            }
+            set
+            {
+                _timeStamp = value;
+            }
+        }
+    }
+}
---
+++ Drink.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreathNDrinkClassLibrary
+{
+    public class Drink
+    {
+        private string _drinkId;
+        private string _name;
+        private bool _alcoholic;
+        private string _instructions;
+        private string _imgThumbUrl;
+        private List<string> _ingredientList = new ();
+        private List<string> _measurementList = new();
+
+        public string DrinkId
+        {
+            get
+            {
+                return _drinkId;
+            }
+            set
+            {
+                _drinkId = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public bool Alcoholic
+        {
+            get
+            {
+                return _alcoholic;
+            }
+            set
+            {
+                _alcoholic = value;
+            }
+        }
+
+        public string Instructions
+        {
+            get
+            {
+                return _instructions;
+            }
+            set
+            {
+                _instructions = value;
+            }
+        }
+
+        public string ImgThumbUrl
+        {
+            get
+            {
+                return _imgThumbUrl;
+            }
+            set
+            {
+                _imgThumbUrl = value;
+            }
+        }
+
+        public void AddIngredientAndMeasurement(string ingredient, string measurement)
+        {
+            _ingredientList.Add(ingredient);
+            _measurementList.Add(measurement);
+        }
+    }
+}
