3775a0b70daa22996b9ac335ad608c6ed456319c
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2021-11-23 11:26:19 +0100

    Added AlcoholVolCalc

--- Drink.cs
+++ Drink.cs
@@ -13,8 +13,8 @@ namespace BreathNDrinkClassLibrary
         private bool _alcoholic;
         private string _instructions;
         private string _imgThumbUrl;
-        private List<string> _ingredientList = new ();
-        private List<string> _measurementList = new ();
+        private List<string> _ingredientList = new();
+        private List<string> _measurementList = new();
 
         public string DrinkId
         {
@@ -98,10 +98,99 @@ namespace BreathNDrinkClassLibrary
             }
         }
 
+        public double AlcoholPercentage
+        {
+            get
+            {
+                return CalculateAlcoholPercentage();
+            }
+            set
+            {
+            }
+        }
+
         public void AddIngredientAndMeasurement(string ingredient, string measurement)
         {
             _ingredientList.Add(ingredient);
             _measurementList.Add(measurement);
+        }
+
+        private double CalculateAlcoholPercentage()
+        {
+            List<double> measurementsInMl = new();
+
+            foreach (string measurement in _measurementList)
+            {
+                if (measurement != null)
+                {
+                    string[] strArray = measurement.Split(' ');
+                    double value = 0.0;
+                    string unit = "";
+                    foreach (string component in strArray)
+                    {
+                        if (int.TryParse(component, out int result))
+                            value += result;
+                        else if (component.Equals("1/2"))
+                            value += 0.5;
+                        else if (component.ToLower().Equals("oz"))
+                            unit = "oz";
+                        else if (component.ToLower().Equals("cup"))
+                            unit = "cup";
+                    }
+
+                    double amountInMl = 0.0;
+                    switch (unit)
+                    {
+                        case "oz":
+                            amountInMl = value * 28.41306;
+                            break;
+                        case "cup":
+                            amountInMl = value * 284.1306;
+                            break;
+                    }
+
+                    measurementsInMl.Add(amountInMl);
+                }
+            }
+
+            List<double> alcoholByVolList = new();
+            foreach (string ingredient in _ingredientList)
+            {
+                double alcoholByVol = 0.0;
+                switch (ingredient.ToLower())
+                {
+                    case "tequila":
+                        alcoholByVol = 0.45;
+                        break;
+                    case "triple sec":
+                        alcoholByVol = 0.3;
+                        break;
+                    case "lime juice":
+                        alcoholByVol = 0.0;
+                        break;
+                }
+
+                alcoholByVolList.Add(alcoholByVol);
+            }
+
+            double totalVol = 0.0;
+            foreach (double measurement in measurementsInMl)
+            {
+                totalVol += measurement;
+            }
+
+            double totalAlcVol = 0.0;
+            int measurementNo = 0;
+            foreach (double measurement in measurementsInMl)
+            {
+                double alcVol = measurement * alcoholByVolList[measurementNo];
+
+                totalAlcVol += alcVol;
+
+                measurementNo++;
+            }
+
+            return totalAlcVol / totalVol;
         }
     }
 }
